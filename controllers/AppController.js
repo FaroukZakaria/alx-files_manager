b6314f4dd292ee8a03142605ce1f31655e48dc2b
FaroukZakaria <GamesWithoutLose97@gmail.com>
2024-03-13 15:38:45 +0000

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,28 @@
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisIsAlive = redisClient.isAlive();
+    const dbIsAlive = dbClient.isAlive();
+
+    if (redisIsAlive && dbIsAlive) {
+      return res.status(200).json({ redis: true, db: true });
+    } else {
+      return res.status(500).json({ redis: false, db: false });
+    }
+  }
+
+  static async getStats(req, res) {
+    try {
+      const nbUsers = await dbClient.nbUsers();
+      const nbFiles = await dbClient.nbFiles();
+      return res.status(200).json({ users: nbUsers, files: nbFiles });
+    } catch (error) {
+      console.error('Error getting stats:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = AppController;
